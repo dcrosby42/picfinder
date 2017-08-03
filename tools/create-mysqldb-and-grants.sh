ddc8fba4628102ce72bd8b62800074e302c332b7
David Crosby <dcrosby@llnw.com>
2017-08-03 07:23:32 -0400

    tweak db tool scripts to use config file for db info

--- tools/create-mysqldb-and-grants.sh
+++ tools/create-mysqldb-and-grants.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 pushd `dirname ${BASH_SOURCE[0]}` > /dev/null; HERE=`pwd`; popd > /dev/null
 cd $HERE
+
+cfg=$HERE/../local-config.yaml
+env=$1
+if [ -z "$env" ]; then
+  env=dev
+fi
+
+set -e
+`picfinder --config $cfg --env $env db shellvars`
+
     
 mysql_cmd="sudo mysql"
 database=picfinder
 username=picfinder
 password=picfinder
-echo "create database $database" | $mysql_cmd
-echo "grant all on ${database}.* to ${username} identified by '${password}'" | $mysql_cmd
-echo "grant all on ${database}.* to ${username}@localhost identified by '${password}'" | $mysql_cmd
+set -x
+echo "create database $DBNAME" | $mysql_cmd
+echo "grant all on ${DBNAME}.* to ${DBUSER} identified by '${DBPASS}'" | $mysql_cmd
+echo "grant all on ${DBNAME}.* to ${DBUSER}@localhost identified by '${DBPASS}'" | $mysql_cmd
--- tools/rebuild-db.sh
+++ tools/rebuild-db.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pushd `dirname ${BASH_SOURCE[0]}` > /dev/null; HERE=`pwd`; popd > /dev/null
+cd $HERE/..
 
 go install && picfinder db rebuild -really
